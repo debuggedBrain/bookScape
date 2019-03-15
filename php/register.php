18891c0691343c57173669c16698a2ce55969d0a
debuggedBrain <ashhelal@gmail.com>
2019-03-15 16:26:54 -0400

    creating directory for PHP files

---
+++ php/register.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require('rmq/testRabbitMQClient.php');
+
+error_reporting(E_ALL);
+ini_set('display_errors','1');
+
+$username = $_POST["regusername"];
+$password = $_POST["regpassword"];
+$response = registration($username, $password);
+
+if ($response != false)
+{
+        header('location:success.php');
+}
+
+else
+{
+	header('location:index.php');
+}
+?>
+
