6754d7855ba4d3da9a202076bc4837480f5631e5
debuggedBrain <ashhelal@gmail.com>
2019-03-17 17:31:10 -0400

    initial PHP file which will make user schedule input into an array ex.['IT202','6-9PM', 'IT220', '8-9:30PM'....]

--- php/schedule.php
+++ php/schedule.php
@@ -5,9 +5,23 @@ require('../rmq/testRabbitMQClient.php');
 error_reporting(E_ALL);
 ini_set('display_errors','1');
 
-$course1 = $_POST["course1"];
-$time1 = $_POST["time1"];
-$response = ($course1, $time1);
+$courseInfo = array()//array holding course info
+$courseInfo['course1'] = $_POST["course1"];
+$courseInfo['time1'] = $_POST["time1"];
+$courseInfo['course2'] = $_POST["course2"];
+$courseInfo['time2'] = $_POST["time2"];
+$courseInfo['course3'] = $_POST["course3"];
+$courseInfo['time3'] = $_POST["time3"];
+$courseInfo['course4'] = $_POST["course4"];
+$courseInfo['time4'] = $_POST["time4"];
+$courseInfo['course5'] = $_POST["course5"];
+$courseInfo['time5'] = $_POST["time5"];
+$courseInfo['course6'] = $_POST["course6"];
+$courseInfo['time6'] = $_POST["time6"];
+$courseInfo['course7'] = $_POST["course7"];
+$courseInfo['time7'] = $_POST["time7"];
+
+$response = buildSchedule($courseInfo);
 
 if ($response != false)
 {
