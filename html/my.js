48350dbad266c84abd86e527dfeec1531832f572
iilego <mal46@njit.edu>
2019-03-19 18:07:38 -0400

    Created files that act on pressing the own button on books.php

--- html/my.js
+++ html/my.js
@@ -9,12 +9,13 @@ function bookse(){
 		success: function(data){
 			console.log(data)
 			for (i=0; i < data.items.length; i++){
-				result.innerHTML += "<h3>"  + data.items[i].volumeInfo.industryIdentifiers[1].type+"<br></h3>"
 				result.innerHTML += "<h3>"  + data.items[i].volumeInfo.industryIdentifiers[1].identifier+"<br></h3>"
 				result.innerHTML += "<h3>" + data.items[i].volumeInfo.title + "<h3>" + "<br>"
 				result.innerHTML += "<h3>" + data.items[i].volumeInfo.authors+ "</h3><br>"
 				result.innerHTML += "<h3>"  + data.items[i].volumeInfo.description+ "<br>"
-				result.innerHTML += "<a href = " + data.items[i].volumeInfo.infoLink + '><button id = "imagebutton" class ="red btn"></button></a>'
+				result.innerHTML += "<a href ='addbutton.php'>" +'<button id="own" name="own" class ="red btn">Own Book</button></a>'
+				result.innerHTML += "<a href = " + data.items[i].volumeInfo.infoLink + '><button id="check" name="check" class ="red btn">Check Offers</button></a>'
+
 			}
 		},
 		type: 'GET'
