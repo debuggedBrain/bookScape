003a7097bba6b57e13e7568657ac1a296718b7e9
Ali Helal <ah366@njit.edu>
2019-03-20 17:30:38 -0400

    Update success.php

--- php/success.php
+++ php/success.php
@@ -2,15 +2,29 @@
 session_start();
 ?>
 <html>
-<h1>Welcome!</h1>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, intiial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet"href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+	integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://bootswatch.com/4/journal/bootstrap.min.css">
+	
+	<link rel="stylesheet" href="login.css" type="text/css">
+	<title>BookScape</title>
+</head>
 <body>
+	<div class="container mt-4">
+	<h1 class="display-4 text-center">
+	<i class="fas fa-book-open text-primary"></i> Book <span class="text-primary"> Scape </span> </h1><br>
+
 	<form action="logout.php" method="post">
 	<input name="logout" type="submit" id="logout" value="logout">
-	</form><br>
+	</form>
 	
-	<form action="../html/scheduleCreate.php">
+	<form action="../html/scheduleCreate.php" method="post">
 	<button type="submit">Create Schedule</button>
-	</form><br>
+	</form>
 
 </body>
 </html>
