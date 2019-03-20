c8e3da1ad684b321638eb50e63f796c815194dd1
Ali Helal <ah366@njit.edu>
2019-03-20 19:20:48 -0400

    added book seller to server

--- rmq/testRabbitMQServer.php
+++ rmq/testRabbitMQServer.php
@@ -27,6 +27,14 @@ function doSchedule($courseInfo)
 	return $schedule->validateSchedule($courseInfo);
 }
 
+
+function doSchedule($bookInfo)
+{
+	$bookSell = new loginDB();
+	return $bookSell->validateSell($bookInfo);
+}
+
+
 function requestProcessor($request)
 {
   echo "NEW: received request".PHP_EOL;
