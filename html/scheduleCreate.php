7b10959b82eb5f65a6de308b1aa64d02130da1a6
Ali Helal <ah366@njit.edu>
2019-03-21 18:33:32 -0400

    Update scheduleCreate.php

--- html/scheduleCreate.php
+++ html/scheduleCreate.php
@@ -2,8 +2,21 @@
 session_start();
 ?>
 <html>
-<head><h1>Schedule Builder</h1></head><br>
-<body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, intiial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet"href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://bootswatch.com/4/journal/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="login.css" type="text/css">
+    <title>BookScape</title>
+</head>
+<body><center>
+    <div class="container mt-4">
+    <h1 class="display-4 text-center">
+    <i class="fas fa-book-open text-primary"></i> Book <span class="text-primary"> Scape </span> </h1><br></div>
 	<form action=../php/schedule.php method="post">
 		<label>Enter username:</label>
 		<input type="text" name="user" style="width: 100px" required><br>
@@ -23,7 +36,7 @@ session_start();
                 <input type="text" name="course7"style="width: 100px"><br>
 		<input type="submit" value="submit">
 	</form>
-</body>
+</center></body>
 </html>
 
 
