693e1379d11bb203d752c0ad51236e1e42bc2e91
Ali Helal <ah366@njit.edu>
2019-03-21 23:21:45 -0400

    Update sellers.php

--- php/sellers.php
+++ php/sellers.php
@@ -4,7 +4,7 @@ session_start();
 require('../rmq/testRabbitMQClient.php');
 error_reporting(E_ALL);
 ini_set('display_errors','1');
-$user = $_POST["user"];
+$user = $_SESSION['user_id'];
 $bookInfo = array();//array holding course info
 $bookInfo['title'] = $_POST["title"];
 $bookInfo['ISBN'] = $_POST["ISBN"];
