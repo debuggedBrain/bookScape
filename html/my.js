2c3f475b0b5c0195debe7762e7c5b1937f6e425d
Ali Helal <ah366@njit.edu>
2019-03-23 17:46:13 -0400

    Update my.js

--- html/my.js
+++ html/my.js
@@ -9,8 +9,8 @@ function bookse(){
 		success: function(data){
 			console.log(data)
 			for (i=0; i < data.items.length; i++){
-				result.innerHTML += "<h3>"  + data.items[i].volumeInfo.industryIdentifiers[1].identifier+"<br></h3>"
-				result.innerHTML += "<h3>" + data.items[i].volumeInfo.title + "<h3>" + "<br>"
+				result.innerHTML += "<h3>"  + data.items[i].volumeInfo.industryIdentifiers[1].identifier+"</h3><br>"
+				result.innerHTML += "<h3>" + data.items[i].volumeInfo.title + "</h3><br>"
 				result.innerHTML += "<h3>" + data.items[i].volumeInfo.authors+ "</h3><br>"
 				//result.innerHTML += "<a href ='addbutton.php'>" +'<button id="own" name="own" class ="red btn">Own Book</button></a>'
 				//result.innerHTML += "<a href = " + data.items[i].volumeInfo.infoLink + '><button id="check" name="check" class ="red btn">Check Offers</button></a>'
