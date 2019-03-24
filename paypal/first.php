d3968c84d98813365a1a4018ea7ad6b408028adf
Ali Helal <ah366@njit.edu>
2019-03-24 15:33:04 -0400

    Create first.php

---
+++ paypal/first.php
@@ -0,0 +1,39 @@
+<?php
+// 1. Autoload the SDK Package. This will include all the files and classes to your autoloader
+require __DIR__  . '/PayPal-PHP-SDK/autoload.php';
+// 2. Provide your Secret Key. Replace the given one with your app clientId, and Secret
+// https://developer.paypal.com/webapps/developer/applications/myapps
+$apiContext = new \PayPal\Rest\ApiContext(
+    new \PayPal\Auth\OAuthTokenCredential(
+        'AYQiDljwS-6LjphSGsinhb30c5Hn7XpgRa7oVqNfTxngABAdOS8_fRI3X4dpfVnKo-cWm26f8DmBaGOf',     // ClientID
+        'EOnIZkCm1RXWNOFMLqbzjpCJjklokWOOrE9cshiAkpg41JIJxtphySU3m5Sl7iqr9XfYsxi3JVXU_YoJ'      // ClientSecret
+    )
+);
+// 3. Lets try to create a Payment
+// https://developer.paypal.com/docs/api/payments/#payment_create
+$payer = new \PayPal\Api\Payer();
+$payer->setPaymentMethod('paypal');
+$amount = new \PayPal\Api\Amount();
+$amount->setTotal('1.00');
+$amount->setCurrency('USD');
+$transaction = new \PayPal\Api\Transaction();
+$transaction->setAmount($amount);
+$redirectUrls = new \PayPal\Api\RedirectUrls();
+$redirectUrls->setReturnUrl("https://google.com")
+    ->setCancelUrl("https://example.com/gmail.com");
+$payment = new \PayPal\Api\Payment();
+$payment->setIntent('sale')
+    ->setPayer($payer)
+    ->setTransactions(array($transaction))
+    ->setRedirectUrls($redirectUrls);
+// 4. Make a Create Call and print the values
+try {
+    $payment->create($apiContext);
+    echo $payment;
+    echo "\n\nRedirect user to approval_url: " . $payment->getApprovalLink() . "\n";
+}
+catch (\PayPal\Exception\PayPalConnectionException $ex) {
+    // This will print the detailed information on the exception.
+    //REALLY HELPFUL FOR DEBUGGING
+    echo $ex->getData();
+}
