b3d19d295e43401e8416e003da040b2fe8c607ad
Ali Helal <ah366@njit.edu>
2019-03-23 21:46:37 -0400

    Update my.js

--- html/my.js
+++ html/my.js
@@ -10,6 +10,7 @@ function bookse(){
 			console.log(data)
 			for (i=0; i < data.items.length; i++){
 				result.innerHTML += "<h3>ISBN#: "  + data.items[i].volumeInfo.industryIdentifiers[0].identifier+"<br></h3>"
+				result.innerHTML += "<h3>ISBN#: "  + data.items[i].volumeInfo.industryIdentifiers[1].identifier+"<br></h3>"
 				result.innerHTML += "<h3>Title: " + data.items[i].volumeInfo.title + "<h3>" + "<br>"
 				result.innerHTML += "<h3>Author: " + data.items[i].volumeInfo.authors+ "</h3><br>"
 				result.innerHTML += "<a href ="+data.items[i].volumeInfo.infoLink +'><button id="b">G Books</button></a><br>----------------------------------<br>'
