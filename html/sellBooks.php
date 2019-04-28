e6686a3880ebe79dfdea333410aea5418f086230
rk559 <35547428+rk559@users.noreply.github.com>
2019-04-28 17:20:43 -0400

    Updated CSS/Images

--- html/sellBooks.php
+++ html/sellBooks.php
@@ -11,6 +11,14 @@ session_start();
     <link rel="stylesheet" href="https://bootswatch.com/4/journal/bootstrap.min.css">
     
     <link rel="stylesheet" href="login.css" type="text/css">
+	<style>
+	body{
+	background-color: #EFEEEE;
+	background: url(images/background2.png);
+	
+	}
+
+	</style>
     <title>BookScape</title>
 </head>
 <body><center>
@@ -19,17 +27,17 @@ session_start();
     <i class="fas fa-book-open text-primary"></i> Book <span class="text-primary"> Scape </span> </h1><br></div>
     <form action=../php/sellers.php method="post">
         <label>Book title:</label>
-        <input type="text" name="title" style="width: 100px" required>
+        <input style ="border-radius: 4px;"type="text" name="title" style="width: 100px" required>
         <label>ISBN #:</label>
-        <input type="text" name="ISBN" style="width: 100px" required>
+        <input style ="border-radius: 4px;" type="text" name="ISBN" style="width: 100px" required>
         <label>Price:</label>
-        <input type="number" step="0.01" name="price" style="width: 100px" required><br>
-        <input type="submit" value="submit">
+        <input style ="border-radius: 4px;" type="number" step="0.01" name="price" style="width: 100px" required><br>
+        <input style = "color: white; background:#B4A8A8; border-radius: 4px; width: 130px; height: 35px;"type="submit" value="submit">
     </form>
 <h3>Search for a book:</h3>
-      <input type = "search" id = "books">
+      <input style ="border-radius: 4px;"type = "search" id = "books">
       <label for "search"><label>
-      <button type = "button" id = "b">g books</button>
+      <button style = "color: white; background:#B4A8A8; border-radius: 4px; width: 130px; height: 35px;" type = "button" id = "b">g books</button>
       <div id="result"></div>
       <script src ="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
       <script src = "my.js"></script>
