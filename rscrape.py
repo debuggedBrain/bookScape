67e0bb3022653c8dd807f1a44e097045d85beafe
officialmaj <sa646@njit.edu>
2019-05-16 08:28:25 -0400

    Setting variable for body by grabbing all individual div tags, ready for parsing and manipulation

--- rscrape.py
+++ rscrape.py
@@ -4,6 +4,10 @@ import re
 from pprint import pprint as pp
 
 URL = "https://sis.rutgers.edu/soc/#courses?subject=003&semester=12019&campus=NK&level=U";
-req = requests.get(URL)
+req = requests.get(URL);
 
-soup = BeautifulSoup(req.text, 'html.parser')
+soup = BeautifulSoup(req.text, 'html.parser');
+
+body = soup.find_all('div');
+
+
