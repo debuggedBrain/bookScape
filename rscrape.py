27f7b4502b09d8675ed6df8b5e4bc23761237f4b
officialmaj <sa646@njit.edu>
2019-05-16 08:31:36 -0400

    importing selenium for dynamic website scraping and automation. Also importing time module for accurate webdriver automation measurement

--- rscrape.py
+++ rscrape.py
@@ -1,7 +1,9 @@
+import time
 from bs4 import BeautifulSoup
 import requests
 import re
 from pprint import pprint as pp
+from selenium import webdriver
 
 URL = "https://sis.rutgers.edu/soc/#courses?subject=003&semester=12019&campus=NK&level=U";
 req = requests.get(URL);
