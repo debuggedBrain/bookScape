dc0b99ab9c8d95288c90df4bdbcf5feedf3580e9
officialmaj <sa646@njit.edu>
2019-05-16 08:21:04 -0400

    setting URL variable directory value equal to that of Rutgers Bookstore

--- rscrape.py
+++ rscrape.py
@@ -2,3 +2,5 @@ from bs4 import BeautifulSoup
 import requests
 import re
 from pprint import pprint as pp
+
+URL = "https://sis.rutgers.edu/soc/#courses?subject=003&semester=12019&campus=NK&level=U"
