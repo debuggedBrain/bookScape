be7aee7b53c7ba87a8efd9c7ff6d7bad523bc30e
officialmaj <sa646@njit.edu>
2019-05-16 08:23:36 -0400

    Making urlopen request to Rutgers Bookstore domain. Setting domain value into BeautifulSoup container for html parser text manipulation

--- rscrape.py
+++ rscrape.py
@@ -3,4 +3,7 @@ import requests
 import re
 from pprint import pprint as pp
 
-URL = "https://sis.rutgers.edu/soc/#courses?subject=003&semester=12019&campus=NK&level=U"
+URL = "https://sis.rutgers.edu/soc/#courses?subject=003&semester=12019&campus=NK&level=U";
+req = requests.get(URL)
+
+soup = BeautifulSoup(req.text, 'html.parser')
